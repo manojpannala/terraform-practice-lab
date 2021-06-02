b0f151c4ca40d873b36f49d7cb0ca47315e49395
manojpannala <manojpannala@outlook.com>
2021-06-02 08:46:59 +0300

    Add Private subnets

--- TF_AWS/VPC/vpc.tf
+++ TF_AWS/VPC/vpc.tf
@@ -46,3 +46,39 @@ resource "aws_subnet" "mrp_subnet_public-3" {
     Name = "mrp_subnet_public-3"
   }
 }
+
+# Create Private Subnet
+resource "aws_subnet" "mrp_subnet_private-1" {
+  vpc_id     = aws_vpc.mrp_vpc.id
+  cidr_block = "10.0.4.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "mrp_subnet_public-1"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "mrp_subnet_private-2" {
+  vpc_id     = aws_vpc.mrp_vpc.id
+  cidr_block = "10.0.5.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name = "mrp_subnet_private-2"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "mrp_subnet_private-3" {
+  vpc_id     = aws_vpc.mrp_vpc.id
+  cidr_block = "10.0.6.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "eu-central-1c"
+
+  tags = {
+    Name = "mrp_subnet_private-3"
+  }
+}
