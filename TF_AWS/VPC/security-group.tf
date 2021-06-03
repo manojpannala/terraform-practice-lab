7f20197fe7d101a4f221f5607e75497e5fea47cb
manojpannala <manojpannala@outlook.com>
2021-06-03 12:25:46 +0300

    fixed SG cidr_blocks

--- TF_AWS/VPC/security-group.tf
+++ TF_AWS/VPC/security-group.tf
@@ -7,14 +7,14 @@ resource "aws_security_group" "allow-ssh" {
       from_port = 0
       to_port = 0
       protocol = "-1"
-      cidr_block = ["0.0.0.0/0"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_block = ["0.0.0.0/0"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
