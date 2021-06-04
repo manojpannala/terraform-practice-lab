2a925fc409e570aab1febcaf58f36ab96c255928
manojpannala <manojpannala@outlook.com>
2021-06-04 12:51:14 +0300

    Added RDS Parameter group

--- TF_AWS_2/rds.tf
+++ TF_AWS_2/rds.tf
@@ -6,3 +6,16 @@ resource "aws_db_subnet_group" "mariadb-subnet" {
   subnet_ids = [ aws_subnet.mrp_subnet_private-1.id, aws_subnet.mrp_subnet_private-2.id  ]
 }
 
+# RDS Parameters
+
+resource "aws_db_parameter_group" "mrp-mariadb-parameter" {
+    name = "mrp-mariadb-parameters"
+    family = "mariadb10.5"
+    description = "MariaDB parameter group"
+
+    parameter {
+      name = "max_allowed_packet"
+      value = "16777216"
+    }
+  
+}
