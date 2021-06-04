a54aae2a45a90085749d438b159e244c4e5bebf6
manojpannala <manojpannala@outlook.com>
2021-06-04 14:23:26 +0300

    fix args

--- TF_AWS_IAM/iamuser.tf
+++ TF_AWS_IAM/iamuser.tf
@@ -21,8 +21,8 @@ resource "aws_iam_group_membership" "admin-users" {
 
 # Policy for AWS Group
 resource "aws_iam_group_policy_attachment" "admin-users-attach" {
-    name = "admin-users-attach"
-    group = [aws_iam_group.admingroup.name]
+    # name = "admin-users-attach"
+    group = aws_iam_group.admingroup.name
     policy_arn = "arn:aws:iam::687326881945:user/kops"
   
 }
