7fa2c4739787634f9b45fe632332e678e6adeb59
manojpannala <manojpannala@outlook.com>
2021-06-04 12:58:30 +0300

    Added RDS Instance properties

--- TF_AWS_2/rds.tf
+++ TF_AWS_2/rds.tf
@@ -16,6 +16,29 @@ resource "aws_db_parameter_group" "mrp-mariadb-parameter" {
     parameter {
       name = "max_allowed_packet"
       value = "16777216"
-    }
-  
+    } 
+}
+
+# RDS Instance properties
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mariadb"
+  engine_version       = "10.5.8"
+  instance_class       = "db.t2.micro"
+  identifier           = "mariadb"
+  name                 = "mariadb"
+  username             = "root"
+  password             = "mariadb141"
+  db_subnet_group_name = aws_db_subnet_group.mariadb-subnet.name
+  parameter_group_name = aws_db_parameter_group.mrp-mariadb-parameter.name
+  skip_final_snapshot  = true
+  multi_az             = "false"
+  vpc_security_group_ids = [aws_security_group.allow-mariadb.id]
+  storage_type         = "gp2"
+  backup_retention_period = 10
+  availability_zone = aws_subnet.mrp_subnet_private-1.availability_zone
+  tags = {
+    Name = "mrp-mariadb"
+  }
 }
