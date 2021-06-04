830300cb01c10dc93b30a7d42bcfcdc81e3435fb
manojpannala <manojpannala@outlook.com>
2021-06-04 14:45:54 +0300

    Policy Attachment for S3 Bucket Role

--- TF_AWS_IAM/iamroles.tf
+++ TF_AWS_IAM/iamroles.tf
@@ -21,3 +21,33 @@ resource "aws_iam_role" "s3-mrp-bucket-role" {
     Name = "s3-mrp-bucket-role"
   }
 }
+
+# Policy Attachment S3 Bucket Role
+
+resource "aws_iam_role_policy" "s3-mrp-bucket-role-policy" {
+  name = "s3-mrp-bucket-role-policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "s3:*"
+        Effect = "Allow"
+        Sid    = ""
+        Resource = [
+            "arn:aws:s3:::mrp-tf-bucket-56",
+            "arn:aws:s3:::mrp-tf-bucket-56/*",
+        ]
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "s3-mrp-bucket-role-policy"
+  }
+}
