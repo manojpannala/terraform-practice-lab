2bbd31896829b67e390225f447b19dd5f95d01b7
manojpannala <manojpannala@outlook.com>
2021-06-04 14:19:05 +0300

    Added IAM users

---
+++ TF_AWS_IAM/iamuser.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_user" "adminuser1" {
+    name = "adminuser1"
+}
+
+resource "aws_iam_user" "adminuser2" {
+    name = "adminuser2"
+}
+
+resource "aws_iam_group" "admingroup" {
+    name = "admingroup"
+}
+
+resource "aws_iam_group_membership" "admin-users" {
+    name = "admin-users"
+    users = [ 
+        aws_iam_user.adminuser1.name,
+        aws_iam_user.adminuser2.name
+     ]
+     group = aws_iam_group.admingroup.name
+}
+
+# Policy for AWS Group
+resource "aws_iam_group_policy_attachment" "admin-users-attach" {
+    name = "admin-users-attach"
+    groups = [aws_iam_group.admingroup.name]
+    policy_arn = "arn:aws:iam::687326881945:user/kops"
+  
+}
