717fbbdbdd3887866e88811034705f759decc05e
manojpannala <manojpannala@outlook.com>
2021-06-06 17:33:30 +0300

    Added variables.tf for createInstance.tf

---
+++ TF_Project_Structure/modules/instances/variables.tf
@@ -0,0 +1,34 @@
+# Variable for Create Instance Module
+variable "public_key_path" {
+  description = "Public key path"
+  default = "~/.ssh/mrp_key.pub"
+}
+
+variable "VPC_ID" {
+    type = string
+    default = ""
+}
+
+variable "ENVIRONMENT" {
+    type    = string
+    default = ""
+}
+
+variable "AWS_REGION" {
+default = "eu-central-1"
+}
+
+variable "AMIS" {
+    type = map
+    default = {
+        eu-central-1 = "ami-0980c5102b5ef10cc"
+    }
+}
+
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "PUBLIC_SUBNETS" {
+  type = list
+}
