dba60e4975e2f5dd0ce49883a1fd65c62bfff8a4
manojpannala <manojpannala@outlook.com>
2021-06-06 09:39:07 +0300

    Added variables and provider.tf

--- TF_Modules/main.tf
+++ TF_Modules/main.tf
@@ -1,3 +1,13 @@
 module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+
+    name = "my-cluster"
+    ami = "ami-0980c5102b5ef10cc"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-a6c342ea"
+
+    tags = {
+        Terraform = "true"
+        Environment = "dev"
+    }
 }
